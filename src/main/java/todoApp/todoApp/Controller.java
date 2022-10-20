5799a439aed4b789c16dc504fa8c821a5a22596b
JungSoyi <soi0125@naver.com>
2022-10-20 22:15:50 +0900

    todoApp!

---
+++ src/main/java/todoApp/todoApp/Controller.java
@@ -0,0 +1,13 @@
+package todoApp.todoApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String example(){
+        return "To-do Application !";
+    }
+}
